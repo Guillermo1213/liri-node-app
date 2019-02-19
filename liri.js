1023c2d92fe2c988a29034e3da24ed328160c389
Guillermo Garcia Jr <Guillermo.jr23@outlook.com>
2019-02-18 21:58:19 -0800

    Added initial file structure, and necesarry keys. Also created a possible if dstatemtent for taking in argument and selecting API to be used.

---
+++ liri.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var concertLookup = 
+
+if(process.argv[2] === "concert"){
+    // take in process.argv[4] as a jointed string due to the possibility of multiple words
+    //lookup with bandsintownAPI
+} else if(process.argv[2] === "spotify"){
+    // take in process.argv[5] as a jointed string due to the possibility of multiple words
+    //lookup with SpotifyAPI
+} else if(process.argv[2] === "movie"){
+    // take in process.argv[4] as a jointed string due to the possibility of multiple words
+    //lookup with OMDBAPI
+} else if(process.argv[2] === "do"){
+    //run whatever search parameters are inside random.txt
+} else{
+    //i have no clue what you're doing, but this is not one my many uses.. Try any of these commands.. 
+    //concert-this <artist/band name here>
+    //spotify-this-song '<song name here>'
+    //movie-this '<movie name here>'
+    //do-what-it-says
+}
