204f07cd248638903a67af35293e484fd2598849
Guillermo Garcia Jr <you@example.com>
2019-02-25 19:29:08 -0800

    Command <do-what-it-says> stopped working.. Well, I guess I didn't have it printing exactly what I wanted, but I did have it reading the random.txt.. Couldn't replicate that anymore.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# liri-node-app
+# Liri Node APP
+
+Wanna know some cool information about your favorite song? Wanna know the next time your favorite band will be performing? OR maybe you wanna know some details about a movie your totally in to? What netter way than to do it all from the comfort of your terminal! Now introducing the Liri Node APP! Language Interpriting and Recognition Interface, this little guy can do any of the commands shown below!
+
+## Usage
+Try any of these commands..
+$concert-this <artist/band name here>
+$spotify-this-song <song name here>
+$movie-this <movie name here>
+$do-what-it-says
+
+
+## Contribution
+Currently just a learning project by yours truly, [Guillermo Garcia Jr.](https://guillermo1213.github.io/Bootstrap-Portfolio/),if you have any questions, comments, or concerns you can find my portfolio by clicking on my name. Thanks for taking time to readme! :kissing_heart:
--- liri.js
+++ liri.js
@@ -4,9 +4,9 @@ var keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 const axios = require('axios');
-var OMDBAPI_Key= '8d4af44c';
-const FsNode = require('fs-node');
-
+var OMDBAPI_Key = '8d4af44c';
+var fs = require('fs');
+var it = "./random.txt";
 var query = process.argv[3];
 
 if (process.argv[2] === "spotify-this-song") {
@@ -20,29 +20,31 @@ if (process.argv[2] === "spotify-this-song") {
     axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp")
         .then(function (response) {
             //convert with Moment.js?
-            return console.log('So get this... ' + query +' is going to play on ' + response.data[0].datetime + ' at the ' + response.data[0].venue.name + ' in ' + response.data[0].venue.city + ", " + response.data[0].venue.country + "! Wanna go?  ;)")
+            return console.log('So get this... ' + query + ' is going to play on ' + response.data[0].datetime + ' at the ' + response.data[0].venue.name + ' in ' + response.data[0].venue.city + ", " + response.data[0].venue.country + "! Wanna go?  ;)")
         })
         .catch(function (error) {
             console.log(error);
         });
-} else if (process.argv[2] === "movie-this"){
-    axios.get('http://www.omdbapi.com/?apikey='+ OMDBAPI_Key + '&t=' + query +'')
-    .then (function (response){
-        console.log(response.data.Title + ', what a great movie..');
-        console.log('It was rated at ' + response.data.imdbRating + ' by IMDB and a ' + response.data.Ratings[1].Value + ' by Rotten Tomatoes.');
-        console.log('This awesome movie was originally filmed in the ' + response.data.Country + ' but is available in these languages: ' + response.data.Language + ".");
-        console.log('Starring amazing actors, like ' + response.data.Actors + ". The movie's plot was introduced as: " + response.data.Plot + "");
-    })
-    .catch(function (error){
-        console.log(error);
+} else if (process.argv[2] === "movie-this") {
+    axios.get('http://www.omdbapi.com/?apikey=' + OMDBAPI_Key + '&t=' + query + '')
+        .then(function (response) {
+            console.log(response.data.Title + ', what a great movie..');
+            console.log('It was rated at ' + response.data.imdbRating + ' by IMDB and a ' + response.data.Ratings[1].Value + ' by Rotten Tomatoes.');
+            console.log('This awesome movie was originally filmed in the ' + response.data.Country + ' but is available in these languages: ' + response.data.Language + ".");
+            console.log('Starring amazing actors, like ' + response.data.Actors + ". The movie's plot was introduced as: " + response.data.Plot + "");
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+} else if (process.argv[2] === "do-what-it-says") {
+    fs.readFile(it, 'utf8', function (contents) {
+        console.log(contents);
     });
-} else if (process.argv[2] === "do-this"){
-
-} else{
-   console.log('I have no clue what you\'re doing, but this is not how I\'m used..');
-   console.log('Try any of these commands..');
-   console.log('concert-this <artist/band name here>');
-   console.log('spotify-this-song <song name here>');
-   console.log('movie-this <movie name here>');
-   console.log('do-what-it-says');
+} else {
+    console.log('I have no clue what you\'re doing, but this is not how I\'m used..');
+    console.log('Try any of these commands..');
+    console.log('concert-this <artist/band name here>');
+    console.log('spotify-this-song <song name here>');
+    console.log('movie-this <movie name here>');
+    console.log('do-what-it-says');
 }
